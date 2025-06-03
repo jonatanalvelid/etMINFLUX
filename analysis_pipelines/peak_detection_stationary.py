a48d0e9bca582812b939339433af7959830d84b6
Jonatan Alvelid <35332627+jonatanalvelid@users.noreply.github.com>
2025-06-03 13:56:18 +0200

    add pipeline for stationary peak detection

---
+++ analysis_pipelines/peak_detection_stationary.py
@@ -0,0 +1,118 @@
+import numpy as np
+from scipy import ndimage as ndi
+from skimage import measure
+import cv2
+import math
+import trackpy as tp
+import pandas as pd
+
+tp.quiet()
+
+def peak_detection_stationary(img, prev_frames=None, binary_mask=None, exinfo=None, presetROIsize=None,
+                       maxfilter_kersize=5, peak_min_dist=7, thresh_abs=10, smoothing_radius=1, 
+                       border_limit=15, init_smooth=1, roi_border=3, roi_th_factor=6, num_prev=4, msm_thresh=0.7):
+    """
+    Common parameters:
+    img - current image,
+    prev_frames - previous image(s)
+    binary_mask - binary mask of the region to consider
+    testmode - to return preprocessed image or not
+    exinfo - pandas dataframe of the detected vesicles and their track ids from the previous frames
+
+    Pipeline specific parameters:
+    maxfilter_kersize - size of kernel for maximum filtering
+    peak_min_dist - minimum distance in pixels between two peaks
+    thresh_abs - low intensity threshold in img_ana of the peaks to consider
+    num_peaks - number of peaks to track
+    smoothing_radius - diameter of Gaussian smoothing of img_ana, in pixels
+    ensure_spacing - to ensure spacing between detected peaks or not (bool 0/1)
+    border_limit - how much of the border to remove peaks from in pixels
+    init_smooth - if to perform an initial smoothing of the raw image or not (bool 0/1)
+    """
+    roi_sizes = False
+
+    if binary_mask is None or np.shape(binary_mask) != np.shape(img):
+        binary_mask = np.ones(np.shape(img)).astype('uint16')
+
+    img = np.array(img).astype('float32')
+    if init_smooth==1:
+        img = ndi.gaussian_filter(img, smoothing_radius)
+    
+    # multiply with binary mask
+    img_ana = img * np.array(binary_mask)
+
+    # Peak_local_max in current image as a combo of opencv and numpy
+    size = int(2 * maxfilter_kersize + 1)
+    img_ana = np.clip(img_ana, a_min=0, a_max=None)
+    img_ana = img_ana.astype('float32')
+    # get filter structuring element
+    footprint = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=[size,size])
+    # maximum filter (dilation + equal)
+    image_max = cv2.dilate(img_ana, kernel=footprint)
+    mask = np.equal(img_ana, np.array(image_max))
+    mask &= np.greater(img_ana, thresh_abs)
+
+    # get coordinates of peaks
+    coordinates = np.nonzero(mask)
+    intensities = img_ana[coordinates]
+    # highest peak first
+    idx_maxsort = np.argsort(-intensities)
+    coordinates = tuple(arr for arr in coordinates)
+    coordinates = np.transpose(coordinates)[idx_maxsort]
+
+    # remove everything on the border (takes ~2-3ms if there are a lot of detected coordinates, but usually this is not the case)
+    imsize = np.shape(img)[0]
+    idxremove = []
+    for idx, coordpair in enumerate(coordinates):
+        if coordpair[0] < border_limit or coordpair[0] > imsize - border_limit or coordpair[1] < border_limit or coordpair[1] > imsize - border_limit:
+            idxremove.append(idx)
+    coordinates = np.delete(coordinates,idxremove,axis=0)
+    
+    coordinates = np.flip(coordinates, axis=1)
+
+    # create initial exinfo, if first analysis frame
+    if exinfo is None:
+        exinfo = pd.DataFrame(columns=['particle','frame','x','y','intensity'])
+    coordinates = coordinates[coordinates[:, 0].argsort()]
+    
+    # extract intensities summed around each coordinate
+    intensity_sum_rad = 3
+    intensities = []
+    for coord in coordinates:
+        intensity = np.sum(img[coord[0]-intensity_sum_rad:coord[0]+intensity_sum_rad+1,coord[1]-intensity_sum_rad:coord[1]+intensity_sum_rad+1])/(2*intensity_sum_rad+1)**2
+        intensities.append(intensity)
+    
+    # add to old list of coordinates
+    if len(exinfo) > 0:
+        timepoint = int(max(exinfo['frame'])+1)
+    else:
+        timepoint = 0
+    if len(coordinates)>0:
+        coords_df = pd.DataFrame(np.hstack((np.array(range(len(coordinates))).reshape(-1,1),timepoint*np.ones(len(coordinates)).reshape(-1,1),coordinates,np.array(intensities).reshape(-1,1))),columns=['particle','frame','x','y','intensity'])
+        tracks_all = pd.concat([exinfo, coords_df])
+    else:
+        tracks_all = exinfo
+    
+    memory_frames = 0
+    track_search_dist = 12
+    coords_events = []
+    if timepoint >= num_prev:
+        tracks_all = tp.link(tracks_all, search_range=track_search_dist, memory=memory_frames, t_column='frame');
+        tracks_use = tracks_all[tracks_all['frame']>timepoint-(num_prev+1)].copy()
+        for particle in np.unique(tracks_use['particle']):
+            particle_track = tracks_use[tracks_use['particle']==particle]
+            if len(particle_track) > num_prev:
+                movement = tp.motion.compute_drift(particle_track)
+                # get the frame-to-frame square movement array in the last num_prev+1 frames
+                sm = [np.sqrt((line1[1]['y']-line2[1]['y'])**2+(line1[1]['x']-line2[1]['x'])**2) for line1, line2 in zip(movement.iterrows(),movement.iloc[1:].iterrows())]
+                if np.mean(sm) < msm_thresh:
+                    print(f'EVENT, frame {timepoint}')
+                    coords_events.append(np.array([int(particle_track.iloc[-1]['x']), int(particle_track.iloc[-1]['y'])]))
+                    print(coords_events)
+                    print('')
+                    #break
+    
+    coords_events = np.array(coords_events)
+    #coords_event = np.flip(coords_event, axis=1)  # seems to be needed in this pipeline
+
+    return coords_events, roi_sizes, img_ana, tracks_all
